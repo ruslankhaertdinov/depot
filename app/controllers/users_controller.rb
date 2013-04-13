b5a12c43e3010605d1b3f9bd162612a4bf0d6f6b
Ruslan Khaertdinov <ruslan.khaertdinov@flatstack.com>
2013-04-13 18:47:43 +0400

    add registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  skip_before_filter :authorize, only: [:new, :create]
+
 
   def index
     @users = User.order(:name)
@@ -36,7 +38,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to users_url, notice: "User #{@user.name} was successfully created." }
+        format.html { redirect_to user_url, notice: "User #{@user.name} was successfully created." }
         format.json { render json: @user, status: :created, location: @user }
       else
         format.html { render action: "new" }
