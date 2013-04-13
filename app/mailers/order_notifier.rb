55cb9d69f6f0ca68f539d808070579f0625e1b13
Ruslan Khaertdinov <ruslan.khaertdinov@flatstack.com>
2013-04-13 19:04:11 +0400

    fixing mailer, begining

--- app/mailers/order_notifier.rb
+++ app/mailers/order_notifier.rb
@@ -1,5 +1,11 @@
 class OrderNotifier < ActionMailer::Base
-  default from: 'Ruslan Khaertdinov <ruslanimos@gmail.com>'
+  default from: 'Store Admin <admin@store.com>'
+
+  def welcome_email(user)
+    @user = user
+    @url  = "http://example.com/login"
+    mail(to: user.email, subject: "Welcome to My Awesome Site")
+  end
 
   def received(order)
     @order = order
