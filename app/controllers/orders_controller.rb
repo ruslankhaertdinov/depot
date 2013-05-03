54fbbd5665bf437483d92ad347f7646981d5dcae
Ruslan Khaertdinov <ruslan.khaertdinov@flatstack.com>
2013-05-03 09:35:08 +0400

    add rescue to mailer

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -50,7 +50,7 @@ class OrdersController < ApplicationController
       if @order.save
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
-        OrderNotifier.received(@order).deliver
+        OrderNotifier.received(@order).deliver rescue
         format.html { redirect_to store_url, notice:
             I18n.t('.thanks') }
         format.json { render json: @order, status: :created, location: @order }
