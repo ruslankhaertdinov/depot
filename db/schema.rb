3f0513cd7f09f12e86f0636b573d4c09fd36095d
Ruslan Khaertdinov <ruslan.khaertdinov@flatstack.com>
2013-05-06 09:18:04 +0400

    add products migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130414150439) do
+ActiveRecord::Schema.define(:version => 20130506050329) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at", :null => false
