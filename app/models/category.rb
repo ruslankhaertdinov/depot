2812e4c39652884acde7b2c960b561c9ea414699
Ruslan Khaertdinov <ruslan.khaertdinov@flatstack.com>
2013-05-12 18:17:58 +0400

    add scope :available

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,8 @@ class Category < ActiveRecord::Base
   has_many :products
   attr_accessible :title
   validates :title, presence: true, uniqueness: true, length: {minimum: 3}
+
+  def self.available
+    Category.joins(:products)
+  end
 end
