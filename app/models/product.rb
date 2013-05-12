e63747246f320e4347d667fde96da7f46fd88b27
Ruslan Khaertdinov <ruslan.khaertdinov@flatstack.com>
2013-05-12 17:48:58 +0400

    fix product form

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ class Product < ActiveRecord::Base
   belongs_to :category
   before_destroy :ensure_not_referenced_by_any_line_item
 
-  attr_accessible :title, :price, :description, :image_url
+  attr_accessible :title, :price, :description, :image_url, :category_id
 
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
