c99f5c8491cd613432e351a75996bd6a47f38d78
Ruslan Khaertdinov <ruslan.khaertdinov@flatstack.com>
2013-05-27 09:53:21 +0400

    add validation for product/new

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,7 +8,7 @@ class Product < ActiveRecord::Base
 
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
-  validates :title, uniqueness: true, length: {minimum: 10}
+  validates :title, uniqueness: true, length: {minimum: 3}
   validates :image_url, allow_blank: true, format: {
       with: %r{\.(gif|jpg|png|jpeg|bmp)\z}i,
       message: 'must be a URL for GIF, JPG, JPEG, BMP or PNG image.'
