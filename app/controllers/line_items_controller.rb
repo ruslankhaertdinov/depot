ad4fe43300343e8b6a3477a5ef4fc3bf705fcb11
Ruslan Khaertdinov <ruslan.khaertdinov@flatstack.com>
2013-05-27 09:56:12 +0400

    skip before filter for decrease

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,5 +1,5 @@
 class LineItemsController < ApplicationController
-  skip_before_filter :authorize, only: :create
+  skip_before_filter :authorize, only: [:create, :decrease]
 
   def index
     @line_items = LineItem.all
